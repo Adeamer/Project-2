3ed03e3d2a828d56457bc75f589df36205c3ecac
Rajni Dua <rajni.dua14@gmail.com>
2021-09-09 10:20:24 +0930

    added models

---
+++ models/plant.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Plant extends Model {}
+
+Plant.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    botanical_name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    sun_exposure: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    mature_size: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    soil_type: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+    date_planted: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    watering_plan: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: DataTypes.NOW,
+    },
+    owner_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "plant",
+  }
+);
+
+module.exports = Plant;
