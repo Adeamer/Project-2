040d7a71a56bec30986eaba4ea479d7254f79b0b
Rajni Dua <rajni.dua14@gmail.com>
2021-09-13 21:11:01 +0930

    successfully created and seed the data, made changes in the plantData.json file to fix the issues with date field

--- models/index.js
+++ models/index.js
@@ -1,27 +1,25 @@
-const User = require("./User");
-const Plant = require("./Plant");
-const Category = require("./Category");
+const User = require('./User');
+const Plant = require('./Plant');
+const Category = require('./Category');
 
 Plant.belongsTo(Category, {
-  foreignKey: "category_id",
-  onDelete: "CASCADE",
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
-Category.hasMany(Plant, {
+/* Category.hasMany(Plant, {
   foreignKey: "category_id",
   onDelete: "CASCADE",
-});
+}); */
 
 User.hasMany(Plant, {
-  foreignKey: "owner_id",
-  onDelete: "CASCADE",
+  foreignKey: 'owner_id',
+  onDelete: 'CASCADE',
 });
 
 Plant.belongsTo(User, {
-  foreignKey: "owner_id",
-  onDelete: "CASCADE",
+  foreignKey: 'owner_id',
+  onDelete: 'CASCADE',
 });
 
-
 module.exports = { User, Plant, Category };
-
